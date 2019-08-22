f608583f347a5f563ec1ca5df8a3211b688eeb3f
Marcilio J C Gomes <marciliojcg@gmail.com>
2019-08-22 15:39:39 -0300

    Implementação de windows service de carga de contratos

---
+++ Noris.Contrato.WindowsService/Integration/GeradorDeContratos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace Noris.Contrato.WindowsService.Integration
+{
+    public sealed class GeradorDeContratos : ServiceControl
+    {
+        private readonly CancellationTokenSource _cancellationTokenSource;
+
+        public GeradorDeContratos()
+        {
+            _cancellationTokenSource = new CancellationTokenSource();
+        }
+
+        public bool Start(HostControl hostControl)
+        {
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    //Gera relatorio
+                    System.Console.WriteLine("Processamento da Rotina");
+                    Thread.Sleep(TimeSpan.FromSeconds(5));
+                }
+            }, _cancellationTokenSource.Token);
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            _cancellationTokenSource.Cancel();
+            _cancellationTokenSource.Dispose();
+            return true;
+        }
+    }
+}
--- Noris.Contrato.WindowsService/Program.cs
+++ Noris.Contrato.WindowsService/Program.cs
@@ -1,15 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Noris.Contrato.WindowsService.Integration;
+using Topshelf;
 
 namespace Noris.Contrato.WindowsService
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            HostFactory.Run(configurator =>
+            {
+                configurator.Service<GeradorDeContratos>(s =>
+                {
+                    s.ConstructUsing(name => new GeradorDeContratos());
+                    s.WhenStarted((service, control) => service.Start(control));
+                    s.WhenStopped((service, control) => service.Stop(control));
+                });
+                configurator.RunAsLocalSystem();
+
+                configurator.SetDescription("Exemplo de integração de chamada Window Service");
+                configurator.SetDisplayName("Integração Contrato Energia");
+                configurator.SetServiceName("IntegracaoContratoEnergia");
+            });
         }
     }
+    
 }
